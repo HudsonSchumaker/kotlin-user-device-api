d573f4cbb9c42c872a2d1cbb46f7db58b391ef89
HudsonSchumaker <hudson.schumaker@me.com>
2023-11-15 21:48:20 +0100

    small change

--- src/main/kotlin/com/schumaker/api/user/view/UserController.kt
+++ src/main/kotlin/com/schumaker/api/user/view/UserController.kt
@@ -1,7 +1,9 @@
 package com.schumaker.api.user.view
 
+import com.schumaker.api.device.view.dto.DeviceDTO
 import com.schumaker.api.user.model.entity.User
 import com.schumaker.api.user.service.UserService
+import com.schumaker.api.user.view.dto.AddressDTO
 import com.schumaker.api.user.view.dto.UserDTO
 import com.schumaker.api.user.view.dto.UserForm
 import io.swagger.v3.oas.annotations.Operation
@@ -16,6 +18,8 @@ import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/users")
@@ -43,7 +47,27 @@ class UserController(private val userService: UserService, private val modelMapp
     fun assignDevice(@RequestParam userId: Long, @RequestParam deviceId: Long): ResponseEntity<UserDTO> {
         val user = userService.assignDevice(userId, deviceId)
 
-        val userDTO = modelMapper.map(user, UserDTO::class.java)
+        val userDTO = UserDTO(
+            id = user.id!!,
+            firstName = user.firstName,
+            lastName = user.lastName,
+            address =   AddressDTO(
+                street = user.address.street,
+                city = user.address.city,
+                number = user.address.number,
+                zipCode = user.address.zipCode,
+                country = user.address.country
+            ),
+            birthday = user.birthday,
+            devices = user.devices.stream().map { d-> DeviceDTO(
+                id = d.id!!,
+                serialNumber = d.serialNumber,
+                uuid =  d.uuid,
+                phoneNumber = d.phoneNumber,
+                model = d.model,
+            )}.collect(Collectors.toList())
+        )
+
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(userDTO)
     }
 
